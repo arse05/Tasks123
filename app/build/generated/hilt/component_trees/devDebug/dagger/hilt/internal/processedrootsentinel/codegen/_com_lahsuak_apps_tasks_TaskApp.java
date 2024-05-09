8232aac4174cc7b8a308658298aaf9423e652de2
arse <arse252005@gmail.com>
2024-05-09 16:21:19 +0300

    Initial commit

---
+++ app/build/generated/hilt/component_trees/devDebug/dagger/hilt/internal/processedrootsentinel/codegen/_com_lahsuak_apps_tasks_TaskApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.lahsuak.apps.tasks.TaskApp"
+)
+public final class _com_lahsuak_apps_tasks_TaskApp {
+}
